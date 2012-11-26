78f321e26856a4c1d605fe1f69848d121895a589
Jason Chaney <jchaney@tenfouragency.com>
2012-11-26 15:58:41 -0800

    added faker

--- application/seeds/projects.php
+++ application/seeds/projects.php
@@ -1,22 +1,26 @@
 <?php
+    require_once path('app') . 'libraries/Faker/src/autoload.php';
 
-class Seed_Projects extends \S2\Seed {
-
-    public function grow()
+    class Seed_Projects extends \S2\Seed
     {
-        $project = new Project;
-        $project->name = 'Project Name One';
-        $project->status = 'Active';
-        $project->client_id = 83;
-        $project->save();
 
+        public function grow()
+        {
+            $faker = Faker\Factory::create();
+
+            for ($i = 0; $i < 10; ++$i) {
+                $project = new Project;
+                $project->name = $faker->name;
+                $project->status = "Active";
+                $project->save();
+            }
+        }
+
+        // This is optional. It lets you specify the order each seed is grown.
+        // Seeds with a lower number are grown first.
+        public function order()
+        {
+            return 100;
+        }
     }
 
-    // This is optional. It lets you specify the order each seed is grown.
-    // Seeds with a lower number are grown first.
-    public function order()
-    {
-        return 100;
-    }
-
-}
