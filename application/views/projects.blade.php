df0aac0e2180ee4a7c1e4936f533e4be16439096
Jason Chaney <jason@creativeacceleration.com>
2012-11-28 21:58:54 -0800

    modifies Bootstrap for 40px padding and adds dynamic content to project list view

--- application/views/projects.blade.php
+++ application/views/projects.blade.php
@@ -10,8 +10,34 @@
 <div id="projectList">
     <ul>
         @foreach ($projects as $project)
-            <li><a href="#">{{$project->name}}</a></li>
+            <li><a href="{{URL::to_route('projects')}}/{{$project->id}}">{{$project->name}}</a></li>
         @endforeach
     </ul>
+</div>
+@endsection
+
+
+@section('content_slide_1_left')
+<h1>{{$projects[0]->name}}</h1>
+
+<h2>{{$projects[0]->po}}</h2>
+
+<div class="overviewModule">
+    <a href="#">
+        <div>Issue Tracking</div>
+    </a>
+
+    <p>This project is currently being tracked.<br>
+        Click the link to go to the tracker
+    </p>
+</div>
+<div class="overviewModule">
+    <a href="#">
+        <div>Code Repository</div>
+    </a>
+
+    <p>This project is currently under version control.<br>
+        The repo name is BLAH
+    </p>
 </div>
 @endsection
