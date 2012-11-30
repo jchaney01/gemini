7f6dc57e425f402a6adf4e58fd945204ab7367e5
Jason Chaney <jason@creativeacceleration.com>
2012-11-30 07:22:05 -0800

    fixes project view from pulling change orders from wrong object

--- application/views/projects.blade.php
+++ application/views/projects.blade.php
@@ -1,9 +1,7 @@
 @layout('layouts.dual_col_3_slide')
 
 @section('content_slide_3_left')
-    @foreach ($changeorders as $changeorder)
-        {{ $changeorder->project->client->company_name }}
-    @endforeach
+
 @endsection
 
 @section('top_right')
